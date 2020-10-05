e2d06e4e4adca151ee1aaeb18bb4e0ce1ff15fe8
KingCosmic <KingCosmicDev@gmail.com>
2020-10-05 02:36:31 -0500

    changed folder structure and removed old code

--- khafile.js
+++ khafile.js
@@ -1,7 +1,7 @@
-let project = new Project('New Project');
-project.addAssets('Assets/**');
-project.addShaders('Shaders/**');
-project.addSources('Sources');
-project.addSources('mixin/lib');
+let project = new Project('Phaser3 Game');
+project.addAssets('assets/**');
+project.addShaders('shaders/**');
+project.addSources('src');
+project.addSources('mixin');
 project.addParameter('--macro addGlobalMetadata("", "@:build(mixin.Mixin.sugar())", true, true, false)');
 resolve(project);
